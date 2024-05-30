503fffb63b73e09d0f16ecd502cc3d5571f8dd6d
MSA <crystalbluetech@gmail.com>
2024-05-30 15:34:31 +0100

    dispatched actions

--- src/customStore.js
+++ src/customStore.js
@@ -1,33 +1,22 @@
+import reducer from "./reducer";
 
-// this is how we define a private property in javascript
-// we can't access this property from outside of this module
-// we can only access this property from the functions defined in this module
-// this is a closure
-// state is a private property
-// getState is a public method
-// we can access state from getState
-function createStore() {
+function createStore(reducer) {
     let state;
+
+    function dispatch(action) {
+        //call the reducer to get new state
+        //notify the subscribers
+        state = reducer(state, action);
+    }
 
     function getState() {
         return state;
     }
 
     return {
+       dispatch, 
        getState
     };
 }
 
-
-// with this we are making the state public
-// we can access state from outside of this module
-// export function createStorePublic() {
-//     let statePublic;
-
-//     return {
-//        statePublic
-//     };
-// }
-
-
-export default createStore();
+export default createStore(reducer);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import store from "./customStore";
+import * as actions from "./actions"
 
-// store.state = 1;
+store.dispatch(actions.bugAdded("Bug 1"));
 
-console.log(store.getState()) // undefined
-
-console.log(store);
+console.log(store.getState());
